4ad81eb8bff5a85fd459b4eafcfabac92bc979e1
Topher Brown <topher200@gmail.com>
2016-09-02 02:29:59 -0400

    remove debug lines from test

--- test.py
+++ test.py
@@ -17,16 +17,6 @@ class TestParse(unittest.TestCase):
         """
         log_lines = main.parse(self.test_data)
 
-        import operator
-        print ''.join(
-            ['%s: %s' % (l.line_number, l.parsed_log_message,)
-             for l in sorted(
-                 log_lines,
-                 key=operator.attrgetter('origin_papertrail_id', 'line_number', ),
-                 reverse=True,
-             )
-            ]
-        )
         self.assertGreater(len(log_lines), 0)
         all([self.assertIsInstance(v, main.LogLine) for v in log_lines])
 
