9fb52a97695471816643b291c1a287a740cb12fd
Topher Brown <topher200@gmail.com>
2016-09-02 00:40:20 -0400

    break out the gzip function
    This will aid in writing tests against file searching functionality

--- main.py
+++ main.py
@@ -122,47 +122,60 @@ def __get_previous_log_lines(circular_buffer, origin_line, origin_program_name, 
             line_number += 1
 
 
-def parse(zipped_filename):
+def parse(file_object):
     """
-        Returns a list of L{LogLine} found in zipped_filename
+        Returns a list of L{LogLine} found in L{file_object}
+
+        L{file_object} is expected to be an open file, but can be anything that generates lines of
+        log files.
     """
+    # We use a circular buffer to keep track of the last few lines. When we find an
+    # AssertionError, we search backwards in the log lines to find the lines previous from that
+    # machine
+    circular_buffer = collections.deque(maxlen=10000)
+    log_lines = []
+    for line in file_object:
+        # see if this line has an important error
+        if 'AssertionError' in line:
+            # we found a match! add it to the list
+            papertrail_id, instance_id, program_name, assertion_error = __parse_log_line(line)
+            log_datetime = ""  # TODO
+            origin_line = LogLine(
+                assertion_error,
+                line,
+                log_datetime,
+                papertrail_id,
+                papertrail_id,
+                0
+            )
+            log_lines.append(origin_line)
+
+            # search backwards to grab the previous 10 traceback lines
+            log_lines.extend(
+                itertools.islice(
+                    __get_previous_log_lines(
+                        circular_buffer,
+                        origin_line,
+                        program_name,
+                        instance_id
+                    ),
+                    10
+                )
+            )
+
+        # now that we're done processing this line, add it to the buffer
+        circular_buffer.append(line)
+
+
+def parse_gzipped_file(zipped_filename):
+    """
+        Opens the gzipped file given by L{zipped_filename} and calls L{parse} on it
+
+        Raises an assert if the filename doesn't match that of a gzipped file.
+    """
+    assert zipped_filename.endswith('.gz')
     with gzip.open(zipped_filename, 'r') as f:
-        # We use a circular buffer to keep track of the last few lines. When we find an
-        # AssertionError, we search backwards in the log lines to find the lines previous from that
-        # machine
-        circular_buffer = collections.deque(maxlen=10000)
-        log_lines = []
-        for line in f:
-            # see if this line has an important error
-            if 'AssertionError' in line:
-                # we found a match! add it to the list
-                papertrail_id, instance_id, program_name, assertion_error = __parse_log_line(line)
-                log_datetime = ""  # TODO
-                origin_line = LogLine(
-                    assertion_error,
-                    line,
-                    log_datetime,
-                    papertrail_id,
-                    papertrail_id,
-                    0
-                )
-                log_lines.append(origin_line)
-
-                # search backwards to grab the previous 10 traceback lines
-                log_lines.extend(
-                    itertools.islice(
-                        __get_previous_log_lines(
-                            circular_buffer,
-                            origin_line,
-                            program_name,
-                            instance_id
-                        ),
-                        10
-                    )
-                )
-
-            # now that we're done processing this line, add it to the buffer
-            circular_buffer.append(line)
+        parse(f)
 
 
 def main():
@@ -174,10 +187,7 @@ def main():
     filename = args.filename
 
     print "running with %s" % filename
-
-    assert filename.endswith('.gz')
-    parse(filename)
-
+    parse_gzipped_file(filename)
     print 'program executed in %s' % datetime.timedelta(seconds=time.time() - start_time)
 
 if __name__ == "__main__":
