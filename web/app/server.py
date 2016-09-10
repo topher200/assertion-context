545006a495addb6ba03ad31ffe7a6ebb0954143e
Topher Brown <topher200@gmail.com>
2016-09-10 04:53:17 -0400

    rename app -> flask_app to remove conflict

--- web/app/server.py
+++ web/app/server.py
@@ -8,17 +8,25 @@ from app import s3
 
 
 # start app
-app = flask.Flask(__name__)
-app.config.from_object('config')
+flask_app = flask.Flask(__name__)
+flask_app.config.from_object('config')
 
 # database
-es = FlaskElasticsearch(app)
+es = FlaskElasticsearch(flask_app)
 
 
-@app.route("/api/parse_s3", methods=['POST'])
+def save_log_line(log_line):
+    """
+    Takes a L{LogLine} and saves it to the database
+    """
+    assert isinstance(log_line, LogLine), (type(log_line), log_line)
+    print es.ping()
+
+
+@flask_app.route("/api/parse_s3", methods=['POST'])
 def parse_s3():
     json_request = flask.request.get_json()
-    app.logger.debug('req: %s', json_request)
+    flask_app.logger.debug('req: %s', json_request)
     if not all(json_request.has_key(k) for k in ('bucket', 'key')):
         return 'missing params', 400
     s3.parse_s3_file(json_request['bucket'], json_request['key'])
@@ -26,4 +34,4 @@ def parse_s3():
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', debug=True)
+    flask_app.run(host='0.0.0.0', debug=True)
