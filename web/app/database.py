ad5ae12049de0e2a0ab970837d72149dbf7e0e61
Topher Brown <topher200@gmail.com>
2016-09-11 15:07:56 -0400

    perform an ES.refresh before searching in tests

--- web/app/database.py
+++ web/app/database.py
@@ -14,14 +14,24 @@ DOC_TYPE = 'logline'
 def save_log_line(es, log_line):
     """
         Takes a L{LogLine} and saves it to the database
+
+        Returns True if successfull
     """
     assert isinstance(log_line, LogLine), (type(log_line), log_line)
     doc = log_line.document()
-    es.index(
+    return es.index(
         index=INDEX,
         doc_type=DOC_TYPE,
         id=log_line.papertrail_id,
         body=doc
+    )
+
+def refresh(es):
+    """
+        Performs an ES refresh. Required to see newly-inserted values when searching
+    """
+    es.indices.refresh(
+        index=INDEX
     )
 
 def num_asserts_per_day(es, date_):
--- web/tests/test_elasticsearch.py
+++ web/tests/test_elasticsearch.py
@@ -16,7 +16,7 @@ from app import logline
 
 class TestElasticSearch(unittest.TestCase):
     def setUp(self):
-        self.es = Elasticsearch()
+        self.es = Elasticsearch("localhost:9200")
         self.log_line = logline.LogLine(
             'AssertionError\n',
             '700594297938165774\t2016-08-12T03:18:39\t2016-08-12T03:18:39Z\t407484803\ti-2ee330b7\t107.21.188.48\tUser\tNotice\tmanager.debug\tAssertionError\n',  #pylint: disable=line-too-long
@@ -36,16 +36,27 @@ class TestElasticSearch(unittest.TestCase):
         )
 
     def test_save_log_line(self):
-        """
-            Check that when we save a log line to the DB we get no errors
-        """
-        database.save_log_line(self.es, self.log_line)
-        # next test will confirm that we've actually saved something
+        self.assertTrue(database.save_log_line(self.es, self.log_line))
+        self.assertTrue(
+            self.es.exists(
+                index=database.INDEX,
+                doc_type=database.DOC_TYPE,
+                id=self.log_line.papertrail_id,
+                params={
+                    'refresh': True,
+                }
+            )
+        )
 
     def test_num_asserts_per_day(self):
         """
             Check that when we search for our new log line we find it.
         """
+        # save the new line
+        self.assertTrue(database.save_log_line(self.es, self.log_line))
+        database.refresh(self.es)
+
+        # test that it exists
         self.assertEqual(
             database.num_asserts_per_day(self.es, self.log_line.timestamp.date()),
             1
