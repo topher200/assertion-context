453902c8ba1e8d980bb671c06f8a9437b8e407de
Topher Brown <topher200@gmail.com>
2016-09-10 23:46:42 -0400

    handle s3 errors

--- web/app/s3.py
+++ web/app/s3.py
@@ -4,6 +4,7 @@
 import tempfile
 
 import boto3
+import botocore
 
 from . import file_parser
 
@@ -12,10 +13,22 @@ def parse_s3_file(bucket, key):
     """
         Downloads the file given described by the params and parses it.
 
-        Returns a list of L{file_parser.LogLine}s
+        Returns a list of L{file_parser.LogLine}s. Returns None on error.
     """
     s3 = boto3.client('s3')
     with tempfile.NamedTemporaryFile('wb', delete=False) as local_file:
-        s3.download_fileobj(bucket, key, local_file)
+        try:
+            s3.download_fileobj(bucket, key, local_file)
+        except botocore.exceptions.ClientError as e:
+            if e.response['Error']['Code'] == '403':
+                print("'403 Forbidden' error when trying to download from s3")
+                print("This happens when the system clock is out of date. Restart the container.")
+                return None
+            elif e.response['Error']['Code'] == '404':
+                print("'404 Not Found' error when trying to download from s3")
+                print("Check your filename")
+                return None
+            print("failed to download file from s3 with unknown error")
+            raise
     # TODO: delete the temp file
     return file_parser.parse_gzipped_file(local_file.name)
--- web/server.py
+++ web/server.py
@@ -34,12 +34,21 @@ def save_log_line(log_line):
 
 @flask_app.route("/api/parse_s3", methods=['POST'])
 def parse_s3():
+    # parse our input
     json_request = flask.request.get_json()
     flask_app.logger.debug('req: %s', json_request)
     if not all(k in json_request for k in ('bucket', 'key')):
         return 'missing params', 400
-    for line in s3.parse_s3_file(json_request['bucket'], json_request['key']):
+
+    # use our powerful parser to check out the requested file
+    log_line_generator = s3.parse_s3_file(json_request['bucket'], json_request['key'])
+    if log_line_generator is None:
+        return 'error accessing s3', 502
+
+    # save the parser output to the database
+    for line in log_line_generator:
         save_log_line(line)
+
     return 'success'
 
 
