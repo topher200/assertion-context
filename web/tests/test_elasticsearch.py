b37020910c2d99c908798ce19c6b138bd73a82a2
Topher Brown <topher200@gmail.com>
2016-09-11 16:12:28 -0400

    add a second logline to our tests

--- web/tests/test_elasticsearch.py
+++ web/tests/test_elasticsearch.py
@@ -17,12 +17,22 @@ from app import logline
 class TestElasticSearch(unittest.TestCase):
     def setUp(self):
         self.es = Elasticsearch("localhost:9200")
-        self.log_line = logline.LogLine(
+        self.log_line0 = logline.LogLine(
             'AssertionError\n',
             '700594297938165774\t2016-08-12T03:18:39\t2016-08-12T03:18:39Z\t407484803\ti-2ee330b7\t107.21.188.48\tUser\tNotice\tmanager.debug\tAssertionError\n',  #pylint: disable=line-too-long
             datetime.datetime(2000, 8, 12, 3, 18, 39),
-            '700594297938165774',
-            '700594297938165774',
+            '700594297938165772',
+            '700594297938165770',
+            0,
+            'i-2ee330b7',
+            'manager.debug',
+        )
+        self.log_line2 = logline.LogLine(
+            'AssertionError\n',
+            '700594297938165774\t2016-08-12T03:18:39\t2016-08-12T03:18:39Z\t407484803\ti-2ee330b7\t107.21.188.48\tUser\tNotice\tmanager.debug\tAssertionError\n',  #pylint: disable=line-too-long
+            datetime.datetime(2000, 8, 12, 3, 18, 39),
+            '700594297938165772',
+            '700594297938165770',
             2,
             'i-2ee330b7',
             'manager.debug',
@@ -32,16 +42,21 @@ class TestElasticSearch(unittest.TestCase):
         self.es.delete(
             index=database.INDEX,
             doc_type=database.DOC_TYPE,
-            id=self.log_line.papertrail_id,
+            id=self.log_line0.papertrail_id,
+        )
+        self.es.delete(
+            index=database.INDEX,
+            doc_type=database.DOC_TYPE,
+            id=self.log_line2.papertrail_id,
         )
 
     def test_save_log_line(self):
-        self.assertTrue(database.save_log_line(self.es, self.log_line))
+        self.assertTrue(database.save_log_line(self.es, self.log_line0))
         self.assertTrue(
             self.es.exists(
                 index=database.INDEX,
                 doc_type=database.DOC_TYPE,
-                id=self.log_line.papertrail_id,
+                id=self.log_line0.papertrail_id,
                 params={
                     'refresh': True,
                 }
@@ -53,11 +68,12 @@ class TestElasticSearch(unittest.TestCase):
             Check that when we search for our new log line we find it.
         """
         # save the new line
-        self.assertTrue(database.save_log_line(self.es, self.log_line))
+        self.assertTrue(database.save_log_line(self.es, self.log_line0))
+        self.assertTrue(database.save_log_line(self.es, self.log_line2))
         database.refresh(self.es)
 
         # test that it exists
         self.assertEqual(
-            database.num_asserts_per_day(self.es, self.log_line.timestamp.date()),
+            database.num_asserts_per_day(self.es, self.log_line2.timestamp.date()),
             1
         )
