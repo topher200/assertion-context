345c22516324609e694a8794744e7758d3e45c8e
Topher Brown <topher200@gmail.com>
2016-09-11 03:25:57 -0400

    expand our ES utility functions

--- web/app/elasticsearch.py
+++ web/app/elasticsearch.py
@@ -1,16 +1,43 @@
+"""
+    Utility functions for performing actions on our ES database.
+
+    For all functions, `es` must be an instance of FlaskElasticsearch
+"""
 from .logline import LogLine
+
+INDEX = 'logline-index'
+DOC_TYPE = 'logline'
 
 
 def save_log_line(es, log_line):
     """
         Takes a L{LogLine} and saves it to the database
-
-        es must be an instance of FlaskElasticsearch
     """
     assert isinstance(log_line, LogLine), (type(log_line), log_line)
     doc = log_line.document()
     es.index(
-        index='logline-index',
-        doc_type='logline',
+        index=INDEX,
+        doc_type=DOC_TYPE,
         body=doc
     )
+
+def num_asserts_per_day(es, date_):
+    """
+        Queries the database for number of asserts in a given day.
+
+        Does this by counting the number of log lines on that day. Only counts lines that are the
+        "origin" line.
+    """
+    query = {
+        "query": {
+            "match_all": {}
+        }
+    }
+    res = es.search(
+        index=INDEX,
+        doc_type=DOC_TYPE,
+        body=query
+    )
+
+    print('got %d hits' % res['hits']['total'])
+    print(res)
