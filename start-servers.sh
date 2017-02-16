de48e795f76e5578e5182854c68932b513564deb
Topher Brown <topher200@gmail.com>
2017-02-16 01:37:00 -0500

    manually stop our web process when restarting server

--- start-servers.sh
+++ start-servers.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
+# first, we manually make sure that the web app is stopped. this is required
+# since we dynamically update its files using a volume; unlike other containers,
+# docker won't notice all the changes we make to its files
+docker-compose stop web
+
+# build and start all services
 docker-compose up -d --build "$@"
