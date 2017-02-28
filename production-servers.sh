7b07fea160e40734d40514cb0525c55054a43c47
Topher Brown <topher200@gmail.com>
2017-02-27 23:09:07 -0500

    add prod start script

--- production-servers.sh
+++ production-servers.sh
@@ -6,4 +6,4 @@
 docker-compose stop web
 
 # build and start all services
-docker-compose -f docker-compose.yaml "$@" up -d --build
+docker-compose -f docker-compose.yaml -f docker-compose.prod.yaml "$@" up -d --build
