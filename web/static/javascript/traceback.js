93713c7bfd4d2998e8b959193fe64825d878f55e
Topher Brown <topher200@gmail.com>
2017-03-30 17:42:57 -0400

    add a js func to create a jira ticket

--- web/static/javascript/traceback.js
+++ web/static/javascript/traceback.js
@@ -14,6 +14,22 @@ function hide_traceback_text(traceback_button){
 }
 
 
+function create_jira_ticket(traceback_button){
+    payload = {
+        traceback: traceback_button.value
+    };
+    $.ajax({
+        type: "POST",
+        url: "/create_jira_ticket",
+        data: JSON.stringify(payload),
+        success: function() {
+            location.reload();
+        },
+        contentType: "application/json"
+    });
+}
+
+
 function restore_all(){
     payload = {};
     $.ajax({
