caee161d516ca7ef7bccefe50dfb9685cc588277
Topher Brown <topher200@gmail.com>
2017-04-22 19:49:12 -0400

    handle celery container restart

--- start-servers.sh
+++ start-servers.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-# first, we manually make sure that the web app is stopped. this is required
-# since we dynamically update its files using a volume; unlike other containers,
-# docker won't notice all the changes we make to its files
-docker-compose stop web
+# first, we manually make sure that the web app containers are stopped. this is
+# required since we dynamically update their files using a volume; unlike other
+# containers, docker won't notice all the changes we make to their files
+docker-compose stop web celery
 
 # build and start all services
 docker-compose -f docker-compose.yaml -f docker-compose.dev.yaml up -d --build "$@"
