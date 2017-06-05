a014d103642ae0ea0016ded31b4b64ca2e54205c
Topher Brown <topher200@gmail.com>
2017-06-05 01:37:42 -0400

    use cool icons for my buttons

--- web/static/javascript/traceback.js
+++ web/static/javascript/traceback.js
@@ -44,10 +44,10 @@ function create_jira_ticket(traceback_button){
     });
 }
 
-function jira_comment(comment_button, issue_key) {
+function jira_comment(comment_button, issue_key, traceback_text) {
     comment_button.disabled = true;  // disable button during processing
     var payload = {
-        traceback_text: comment_button.value,
+        traceback_text: traceback_text,
         issue_key: issue_key
     };
     $.ajax({
