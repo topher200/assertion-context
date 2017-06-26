6c89d91219d66957493e459ee882cb66c8a50717
Topher Brown <topher200@gmail.com>
2017-06-26 13:17:44 -0400

    move print statements to 'logger'

--- web/app/s3.py
+++ web/app/s3.py
@@ -1,6 +1,7 @@
 """
     Parse a file living on s3
 """
+import logging
 import tempfile
 
 import boto3
@@ -8,6 +9,9 @@ import botocore
 
 from . import file_parser
 from . import retry
+
+
+logger = logging.getLogger()
 
 
 @retry.Retry(exceptions=(EOFError,))
@@ -23,13 +27,15 @@ def parse_s3_file(bucket, key):
             s3.download_fileobj(bucket, key, local_file)
         except botocore.exceptions.ClientError as e:
             if e.response['Error']['Code'] == '403':
-                print("'403 Forbidden' error when trying to download from s3")
-                print("This happens when the system clock is out of date. Restart the container.")
+                logger.warning("'403 Forbidden' error when trying to download from s3")
+                logger.warning(
+                    "This happens when the system clock is out of date. Restart the container."
+                )
                 return None
             elif e.response['Error']['Code'] == '404':
-                print("'404 Not Found' error when trying to download from s3")
-                print("Check your filename")
+                logger.info("'404 Not Found' error when trying to download from s3")
+                logger.info("Check your filename")
                 return None
-            print("failed to download file from s3 with unknown error")
+            logger.error("failed to download file from s3 with unknown error")
             raise
         return file_parser.parse_gzipped_file(local_file.name)
