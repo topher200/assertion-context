d4a6a9c4a41682156e8548f319500de251bfdb6a
Topher Brown <t.brown@wordstream.com>
2017-07-10 22:28:01 -0400

    add helper function to differ

--- differ/run.sh
+++ differ/run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 diff <(sed -e 's/^.*debug:  //' diff1.txt) <(sed -e 's/^.*debug:  //' diff2.txt)
+# diff <(sed -e 's/^.*debug:  //' diff1.txt | grep token) <(sed -e 's/^.*debug:  //' diff2.txt | grep token)
 
