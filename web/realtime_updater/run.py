8b25bc8a40d97f62c14a2136f565cb636fd0475f
Topher Brown <t.brown@wordstream.com>
2017-07-21 01:35:20 -0400

    add json_parser

--- web/realtime_updater/run.py
+++ web/realtime_updater/run.py
@@ -11,7 +11,7 @@ import sys
 sys.path.append(ROOT)
 
 from app import (
-    file_parser,
+    json_parser,
     logging_util,
     traceback_database
 )
@@ -35,7 +35,7 @@ def main():
 
     # read from the stdout buffer, forever
     count = 0
-    for tb in file_parser.parse(papertrail.stdout):
+    for tb in json_parser.parse_json_stream(papertrail.stdout):
         count += 1
         logger.info('found traceback. #%s', count)
         traceback_database.save_traceback(ES, tb)
