59a8df9e49954a90a4c7d1191528f0ab8c5648c3
Topher Brown <t.brown@wordstream.com>
2017-10-07 12:22:19 -0400

    add a retry to papertrail cli

--- web/realtime_updater/run.py
+++ web/realtime_updater/run.py
@@ -3,6 +3,7 @@ import logging
 import os
 import subprocess
 import tempfile
+import time
 
 from elasticsearch import Elasticsearch
 
@@ -32,25 +33,40 @@ def main():
     start_time, end_time = __get_times()
     logger.info('getting logs from %s -> %s', start_time, end_time)
 
-    # fill a log file with papertrail output
-    with tempfile.NamedTemporaryFile('wb') as local_file:
-        papertrail = subprocess.run(
-            ['/usr/local/bin/papertrail', '--min-time', str(start_time), '--max-time', str(end_time), '-j'],
-            stdout=local_file,
-            encoding="utf-8"
-        )
+    # fill a log file with papertrail output. try 5 times
+    for _ in range(5):
+        local_file = call_papertrail_cli(start_time, end_time)
+        if local_file is not None:
+            break
+        time.sleep(1)
+    if local_file is None:
+        logger.error('papertrail cli failed. %s -> %s', start_time, end_time)
+        return
 
-        count = 0
-        for tb in json_parser.parse_json_file(local_file.name):
-            count += 1
-            logger.info('found traceback. #%s', count)
-            traceback_database.save_traceback(ES, tb)
+    count = 0
+    for tb in json_parser.parse_json_file(local_file.name):
+        count += 1
+        logger.info('found traceback. #%s', count)
+        traceback_database.save_traceback(ES, tb)
 
     logger.info('done with logs from %s -> %s', start_time, end_time)
 
 
 def call_papertrail_cli(start_time, end_time):
-    pass
+    local_file = tempfile.NamedTemporaryFile('wb')
+    res = subprocess.run(
+        ['/usr/local/bin/papertrail', '--min-time', str(start_time), '--max-time', str(end_time), '-j'],
+        stdout=local_file,
+        stderr=subprocess.PIPE,
+        encoding="utf-8"
+    )
+
+    if res.stderr:
+        logger.info('subprocess failed. err: %s', res.stderr[:50])
+        logger.debug('subprocess failed. full err: %s', res.stderr)
+        return None
+    else:
+        return local_file
 
 
 def setup_logging(*_, **__):
@@ -61,8 +77,8 @@ def __get_times():
     now = datetime.datetime.now()
     # lag behind by 5 minutes
     end_time = time_util.round_time(now - datetime.timedelta(minutes=5))
-    # 1 minutes worth of data at a time
-    start_time = end_time - datetime.timedelta(minutes=1)
+    # 5 minutes worth of data at a time
+    start_time = end_time - datetime.timedelta(minutes=5)
     return (start_time, end_time)
 
 
