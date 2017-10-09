3f7c08727f7604055cf09ae2242e8055315e3600
Topher Brown <t.brown@wordstream.com>
2017-10-09 03:37:22 -0400

    kill realtime_updater, since it doesn't die quickly

--- start-local-servers.sh
+++ start-local-servers.sh
@@ -3,7 +3,8 @@
 # first, we manually make sure that the web app containers are stopped. this is
 # required since we dynamically update their files using a volume; unlike other
 # containers, docker won't notice all the changes we make to their files
-docker-compose stop web celery realtime_updater
+docker-compose kill realtime_updater
+docker-compose stop web celery
 
 # build and start all services
 docker-compose -f docker-compose.yaml -f docker-compose.dev.yaml up -d --build "$@"
