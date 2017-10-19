d3d00c8310d3d3c8366fb5bfa4813b98b89ae436
Topher Brown <t.brown@wordstream.com>
2017-10-19 12:12:48 -0400

    add retries to 'bulk' call

--- web/app/ddl/api_call_db.py
+++ web/app/ddl/api_call_db.py
@@ -35,7 +35,7 @@ def save(es, api_calls):
         Returns True if successful
     """
     assert isinstance(api_calls, collections.Iterable), (type(api_calls), api_calls)
-    elasticsearch.helpers.bulk(es, _create_documents(api_calls))
+    elasticsearch.helpers.bulk(es, _create_documents(api_calls), max_retries=5, chunk_size=100)
     invalidate_cache()
     return True
 
