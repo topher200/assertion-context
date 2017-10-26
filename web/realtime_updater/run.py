7930ca7018883afe6874698c24d4b31c482c4f0e
Topher Brown <t.brown@wordstream.com>
2017-10-26 00:45:34 -0400

    stop lagging behind; run in real time

--- web/realtime_updater/run.py
+++ web/realtime_updater/run.py
@@ -94,9 +94,7 @@ def setup_logging(*_, **__):
 
 def __get_times(end_time=None):
     if end_time is None:
-        now = datetime.datetime.now()
-        # lag behind by 3 minutes
-        end_time = time_util.round_time(now - datetime.timedelta(minutes=3))
+        end_time = time_util.round_time(datetime.datetime.now())
 
     # 1 minute worth of data at a time
     start_time = end_time - datetime.timedelta(minutes=1)
