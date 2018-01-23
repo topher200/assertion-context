c130afa44bb9c9f6e59a4fb058c1177360c91181
Topher Brown <t.brown@wordstream.com>
2018-01-23 02:05:25 -0500

    add jaeger to docker-compose

--- web/app/tracing.py
+++ web/app/tracing.py
@@ -4,7 +4,9 @@ import jaeger_client
 def initialize_tracer():
     config = jaeger_client.Config(
         config = {
-            'sampler': {'type': 'const', 'param': 1}
+            'sampler': {'type': 'const', 'param': 1},
+            'logging': True,
+            'local_agent': {'reporting_host': 'jaeger'},
         },
         service_name='hello-world'
     )
