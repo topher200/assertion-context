ad70f2920fcd04f6f0c52dd4a19ea6e4f7e84348
Topher Brown <t.brown@wordstream.com>
2018-04-27 19:31:16 -0400

    add opentracing_instrumentation lib

--- web/app/tracing.py
+++ web/app/tracing.py
@@ -1,7 +1,10 @@
+from opentracing_instrumentation.client_hooks import install_all_patches
 import jaeger_client
 
 
 def initialize_tracer():
+    install_all_patches()
+
     config = jaeger_client.Config(
         config = {
             'sampler': {'type': 'const', 'param': 1},
