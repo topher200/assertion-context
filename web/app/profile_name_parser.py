adcac2ab1fbf5463c6dcda2d73c906eb093b4d98
Topher Brown <t.brown@wordstream.com>
2018-04-28 05:40:48 -0400

    unify save logic

--- web/app/profile_name_parser.py
+++ web/app/profile_name_parser.py
@@ -24,6 +24,8 @@ def parse(traceback: Traceback) -> Traceback:
     index = __find_first_error_line(precursor_lines)
     if index is None: return None
 
+    profile_name = None
+    user_name = None
     if 'update.debug' in traceback.program_name:
         # Apr 16 23:37:09 i-dskfj-j update.debug:  16/Apr/2018:23:37:09.674 23502/#upd:qa-jgon_0918-aws2:3fab             : ERROR    w.update: Failed to update profile `qa-jgon_0918-aws2' (145 of 226)
         # Apr 16 23:37:09 i-dskfj-j update.debug:  Traceback (most recent call last):
@@ -31,17 +33,15 @@ def parse(traceback: Traceback) -> Traceback:
         # the error line also contains our profile name
         match = re.search('#upd:(\S+?):', precursor_lines[index])
         if not match: return None
-        profile_name = match.groups()[0]
+        potential_profile_name = match.groups()[0]
 
         # in some cases, we can get a user name instead of a profile name. check
-        if '@' in profile_name:
-            traceback.user_name = profile_name
-            return traceback
-
-        # ok, it's a profile name
-        traceback.profile_name = profile_name
-        return traceback
-    if 'activity-worker' in traceback.program_name:
+        if '@' in potential_profile_name:
+            user_name = profile_name
+        else:
+            # ok, it's a profile name
+            profile_name = potential_profile_name
+    elif 'activity-worker' in traceback.program_name:
         # Mar 05 15:26:24 i-ksdfj-e swf.quickstart.activity-worker:  05/Mar/2018:15:26:24.462 7339/#prod!310595!AW!quick_start:beekeeping_cc_28910:HB: DEBUG    utils.swf.activity_worker.SwfHeartbeatGenerator: STOP
         # Mar 05 15:26:24 i-ksdfj-e swf.quickstart.activity-worker:  05/Mar/2018:15:26:24.462 7339/#prod!310595!AW!quick_start:beekeeping_cc_28910: ERROR    utils.swf.activity_worker.AbstractSwfActivityWorker: Caught exception while processing activity task, failing
         # Mar 05 15:26:24 i-ksdfj-e swf.quickstart.activity-worker:  Traceback (most recent call last):
@@ -53,8 +53,6 @@ def parse(traceback: Traceback) -> Traceback:
         match = re.search(':(\S+):\s+ERROR', precursor_lines[index])
         if not match: return None
         profile_name = match.groups()[0]
-        traceback.profile_name = profile_name
-        return traceback
     if 'engine.server.debug' in traceback.program_name or 'manager.debug' in traceback.program_name:
         # Mar 20 18:20:50 i-kjsdf-g aws2.engine.server.debug:  20/Mar/2018:18:20:50.834 15165/WS#ttt-solutions-ttt-res-admin@ttt-solutions.com: DEBUG    w.services: f,152158 4424.1889 ChangesHandler (POST) took 26645 milliseconds to complete and final memory 417MB (delta 0MB)
         # Mar 20 18:20:50 i-kjsdf-g aws2.engine.server.debug:  20/Mar/2018:18:20:50.834 15165/MainThread : ERROR    w.services: Unexpected error 500 Internal Server Error
@@ -87,18 +85,17 @@ def parse(traceback: Traceback) -> Traceback:
         profile_name = match.groups()[0]
         user_name = match.groups()[1]
 
-        # modify the traceback if we found anything
-        modified = False
-        if profile_name:
-            traceback.profile_name = profile_name
-            modified = True
-        if user_name:
-            traceback.user_name = user_name
-            modified = True
-        if not modified: return None
-        return traceback
-    else:
+    # modify the traceback if we found anything
+    modified = False
+    if profile_name:
+        traceback.profile_name = profile_name
+        modified = True
+    if user_name:
+        traceback.user_name = user_name
+        modified = True
+    if modified:
         return None
+    return traceback
 
 
 def __strip_traceback_text(log_lines: typing.List[str]) -> typing.Optional[typing.List[str]]:
