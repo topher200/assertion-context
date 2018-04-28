9533ef97e7ac36a6f914b8dc37a1b56660080a56
Topher Brown <t.brown@wordstream.com>
2018-04-28 05:18:06 -0400

    profile names are covered for more programs

--- web/app/profile_name_parser.py
+++ web/app/profile_name_parser.py
@@ -24,7 +24,7 @@ def parse(traceback: Traceback) -> Traceback:
     index = __find_first_error_line(precursor_lines)
     if index is None: return None
 
-    if traceback.program_name == 'update.debug':
+    if 'update.debug' in traceback.program_name:
         # Apr 16 23:37:09 i-dskfj-j update.debug:  16/Apr/2018:23:37:09.674 23502/#upd:qa-jgon_0918-aws2:3fab             : ERROR    w.update: Failed to update profile `qa-jgon_0918-aws2' (145 of 226)
         # Apr 16 23:37:09 i-dskfj-j update.debug:  Traceback (most recent call last):
 
@@ -41,7 +41,21 @@ def parse(traceback: Traceback) -> Traceback:
         # ok, it's a profile name
         traceback.profile_name = profile_name
         return traceback
-    if 'engine.server.debug' in traceback.program_name:
+    if 'activity-worker' in traceback.program_name:
+        # Mar 05 15:26:24 i-ksdfj-e swf.quickstart.activity-worker:  05/Mar/2018:15:26:24.462 7339/#prod!310595!AW!quick_start:beekeeping_cc_28910:HB: DEBUG    utils.swf.activity_worker.SwfHeartbeatGenerator: STOP
+        # Mar 05 15:26:24 i-ksdfj-e swf.quickstart.activity-worker:  05/Mar/2018:15:26:24.462 7339/#prod!310595!AW!quick_start:beekeeping_cc_28910: ERROR    utils.swf.activity_worker.AbstractSwfActivityWorker: Caught exception while processing activity task, failing
+        # Mar 05 15:26:24 i-ksdfj-e swf.quickstart.activity-worker:  Traceback (most recent call last):
+
+        # Mar 16 09:06:32 i-jksdfj-g swf.reporting.activity-worker:  16/Mar/2018:09:06:32.572 7392/#SCH-53f16ea3-f310-4adb-bdc5-cc767f85a0a2:topherbrown: ERROR    engine.services.wordstream.swf_reporting.activity_worker.ReportingSwfActivityWorker:
+        # Mar 16 09:06:32 i-jksdfj-g swf.reporting.activity-worker:  Traceback (most recent call last):
+
+        # the error line also contains our profile name
+        match = re.search(':(\S+):\s+ERROR', precursor_lines[index])
+        if not match: return None
+        profile_name = match.groups()[0]
+        traceback.profile_name = profile_name
+        return traceback
+    if 'engine.server.debug' in traceback.program_name or 'manager.debug' in traceback.program_name:
         # Mar 20 18:20:50 i-kjsdf-g aws2.engine.server.debug:  20/Mar/2018:18:20:50.834 15165/WS#ttt-solutions-ttt-res-admin@ttt-solutions.com: DEBUG    w.services: f,152158 4424.1889 ChangesHandler (POST) took 26645 milliseconds to complete and final memory 417MB (delta 0MB)
         # Mar 20 18:20:50 i-kjsdf-g aws2.engine.server.debug:  20/Mar/2018:18:20:50.834 15165/MainThread : ERROR    w.services: Unexpected error 500 Internal Server Error
         # Mar 20 18:20:50 i-kjsdf-g aws2.engine.server.debug:  Traceback (most recent call last):
@@ -50,6 +64,10 @@ def parse(traceback: Traceback) -> Traceback:
         # Mar 22 09:36:51 i-kdfjk-g aws2.engine.server.debug:  22/Mar/2018:09:36:51.617 15176/MainThread                              : DEBUG    w.services: s,1521725811.6175 ChangesHandler (GET) starting memory 605MB
         # Mar 22 09:36:51 i-kdfjk-g aws2.engine.server.debug:  22/Mar/2018:09:36:51.615 17105/MainThread                              : ERROR    w.services: Unexpected error 500 Internal Server Error
         # Mar 22 09:36:51 i-kdfjk-g aws2.engine.server.debug:  Traceback (most recent call last):
+
+        # Apr 01 07:12:16 i-kdsfj-h manager.debug:  01/Apr/2018:07:12:16.992 30740/PV#hoper-brown-topher@topherland.com:     DEBUG    wordstream.services: f,1522581136.9207 ChangesHandler (GET) took 72 milliseconds to complete and final memory 241MB (delta 0MB)
+        # Apr 01 07:12:16 i-kdsfj-h manager.debug:  01/Apr/2018:07:12:16.992 30740/MainThread                              : ERROR    wordstream.services: Unexpected HTTP Exception
+        # Apr 01 07:12:16 i-kdsfj-h manager.debug:  Traceback (most recent call last):
 
         # we can use the error line to the process PID
         pid_match = re.search('\s(\d+)/MainThread', precursor_lines[index])
@@ -62,14 +80,12 @@ def parse(traceback: Traceback) -> Traceback:
             if pid in precursor_lines[index2]:
                 break
         if index2 is None: return None
-        print('match line: %s' % precursor_lines[index2])
 
         # grab the profile name and user name
-        match = re.search('/WS#(\S+?)-(\w*@\S+)\s*:', precursor_lines[index2])
+        match = re.search('/(?:WS|PV)#(\S+)-(\S*@\S+)\s*:', precursor_lines[index2])
         if not match: return None
         profile_name = match.groups()[0]
         user_name = match.groups()[1]
-        print('profile name: %s, user_name: %s' % (profile_name, user_name))
 
         # modify the traceback if we found anything
         modified = False
