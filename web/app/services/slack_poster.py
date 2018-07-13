bbedc7c95185c1b24863b4b8d517599103e5ff99
Topher Brown <t.brown@wordstream.com>
2018-07-13 19:53:01 -0400

    add slack_poster class

---
+++ web/app/services/slack_poster.py
@@ -0,0 +1,28 @@
+import logging
+import json
+
+import requests
+
+
+from .. import config_util
+from ..traceback import Traceback
+
+WEBHOOK_URL = config_util.get('SLACK_WEBHOOK')
+
+logger = logging.getLogger()
+
+
+class SlackPoster():
+    def post_traceback(traceback:Traceback):
+        slack_data = {'text': traceback.traceback_plus_context_text}
+
+        response = requests.post(
+            WEBHOOK_URL,
+            data=json.dumps(slack_data),
+            headers={'Content-Type': 'application/json'}
+        )
+        if response.status_code != 200:
+            logger.error(
+                'Request to slack returned an error %s, the response is:\n%s',
+                response.status_code, response.text
+            )
