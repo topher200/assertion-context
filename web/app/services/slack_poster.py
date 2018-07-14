8300168d43f8412f27a878ee28dd7e4284ab506a
Topher Brown <t.brown@wordstream.com>
2018-07-13 20:17:44 -0400

    import fix

--- web/app/services/slack_poster.py
+++ web/app/services/slack_poster.py
@@ -12,17 +12,16 @@ WEBHOOK_URL = config_util.get('SLACK_WEBHOOK')
 logger = logging.getLogger()
 
 
-class SlackPoster():
-    def post_traceback(traceback:Traceback):
-        slack_data = {'text': traceback.traceback_plus_context_text}
+def post_traceback(traceback:Traceback):
+    slack_data = {'text': traceback.traceback_plus_context_text}
 
-        response = requests.post(
-            WEBHOOK_URL,
-            data=json.dumps(slack_data),
-            headers={'Content-Type': 'application/json'}
+    response = requests.post(
+        WEBHOOK_URL,
+        data=json.dumps(slack_data),
+        headers={'Content-Type': 'application/json'}
+    )
+    if response.status_code != 200:
+        logger.error(
+            'Request to slack returned an error %s, the response is:\n%s',
+            response.status_code, response.text
         )
-        if response.status_code != 200:
-            logger.error(
-                'Request to slack returned an error %s, the response is:\n%s',
-                response.status_code, response.text
-            )
