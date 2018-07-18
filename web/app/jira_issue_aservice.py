95289dee24ff457b11f69b248626ba0981f52164
Topher Brown <t.brown@wordstream.com>
2018-07-17 22:06:26 -0400

    assign epic link directly

--- web/app/jira_issue_aservice.py
+++ web/app/jira_issue_aservice.py
@@ -184,7 +184,7 @@ def create_jira_issue(title:str, description:str, assign_to:AssignToTeam) -> str
         component = 'Social'
         # epic links are set strangely. this value will not carry over to different Jiras
         # see https://community.atlassian.com/t5/Answers-Developer-Questions/Link-to-Epic-in-rest-api-issue-resource/qaq-p/562851
-        fields['customfield_10008'] = {'value': 'Social Triage'}
+        fields['customfield_10008'] = 'Social Triage'
     if assignee:
         fields['assignee'] = {'name': assignee}
         fields['components'] = [{'name': component}]
