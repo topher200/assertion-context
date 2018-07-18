703f1c02809b3322fffa7097253ea84074758e2a
Topher Brown <t.brown@wordstream.com>
2018-07-18 15:57:20 -0400

    another try at updating attachments of existing messages

--- web/server.py
+++ web/server.py
@@ -381,9 +381,10 @@ def slack_callback():
 
             # send the message back, without the "Create a Ticket" message
             original_message = payload['original_message']
-            original_message['attachments'].pop()
-            logger.info('sending back Slack message: %s', original_message)
-            return flask.jsonify(original_message)
+            response_url = payload['response_url']
+            original_message['attachments'].pop() # destructive!
+            logger.info('sending back Slack message without attachment: %s', original_message)
+            slack_poster.send_updated_message(response_url, original_message)
         except api_aservice.IssueAlreadyExistsError as e:
             # we must post the message as a real user so Jirabot picks it up
             slack_poster.post_message_to_slack_as_real_user(str(e))
