db2effc925310de5b841cdcc8673a9d8e157e211
Topher Brown <t.brown@wordstream.com>
2018-07-19 13:00:30 -0400

    more verbose logic for "No Recent Issues"

--- web/app/api_aservice.py
+++ web/app/api_aservice.py
@@ -95,12 +95,15 @@ def get_tracebacks_for_day(
     elif filter_text == 'No Ticket':
         tb_meta = [tb for tb in tb_meta if not tb.jira_issues]
     elif filter_text == 'No Recent Ticket':
-        tb_meta = [
-            tb for tb in tb_meta if not [
-                issue for issue in tb.jira_issues
-                if issue.updated > TWO_WEEKS_AGO
-            ]
-        ]
+        new_tb_meta = []
+        for tb in tb_meta:
+            recent_issues = []
+            for issue in tb.jira_issues:
+                if issue.updated > TWO_WEEKS_AGO:
+                    recent_issues.append(issue)
+            if recent_issues:
+                new_tb_meta.append(tb)
+        tb_meta = new_tb_meta
     elif filter_text == 'Has Open Ticket':
         tb_meta = [
             tb for tb in tb_meta if
