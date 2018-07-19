844e90e753e862b4d62a75e72121ff43899b1d04
Topher Brown <t.brown@wordstream.com>
2018-07-19 13:21:30 -0400

    TEMP add more debug logging

--- web/app/api_aservice.py
+++ web/app/api_aservice.py
@@ -102,7 +102,9 @@ def get_tracebacks_for_day(
                 if issue.updated > TWO_WEEKS_AGO:
                     has_recent_issues = True
                     break
-            logger.info('traceback: %s: %s, has_recent_issues: %s', tb.traceback.origin_papertrail_id, tb.traceback.origin_timestamp, has_recent_issues)
+            if tb.traceback.origin_papertrail_id == '956637974106402832':
+                logger.info('traceback: %s: %s, has_recent_issues: %s', tb.traceback.origin_papertrail_id, tb.traceback.origin_timestamp, has_recent_issues)
+                logger.info('tb: %s', tb)
             if not has_recent_issues:
                 tb_meta_without_recent_ticket.append(tb)
         tb_meta = tb_meta_without_recent_ticket
