afe5cd4d52282d707eef267705ab96b8d0165cc7
Topher Brown <t.brown@wordstream.com>
2018-11-20 00:41:05 -0500

    correct assert - response is a list

--- web/app/services/fullstory.py
+++ web/app/services/fullstory.py
@@ -57,7 +57,7 @@ def get_link_to_session_at_traceback_time(t:Traceback) -> Optional[str]:
         sessions = None
         response = requests.get(url, headers=headers)
         sessions = response.json()
-        assert isinstance(sessions, dict), sessions
+        assert isinstance(sessions, list), sessions
         assert all(isinstance(s, dict) for s in sessions), sessions
         assert all('CreatedTime' in s for s in sessions), sessions
     except Exception:
