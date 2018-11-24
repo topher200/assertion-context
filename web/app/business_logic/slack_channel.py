e78a319650f1f7d29adbdd32d633b2b474aea83a
Topher Brown <t.brown@wordstream.com>
2018-11-24 12:24:37 -0500

    add business_logic switch for slack channel decider

---
+++ web/app/business_logic/slack_channel.py
@@ -0,0 +1,15 @@
+from ..traceback import Traceback
+
+
+def get(traceback:Traceback):
+    """
+        Given a traceback, returns the name of the slack channel it belongs in.
+
+        We look in the traceback's text for certain trigger words.
+    """
+    if 'facebook' in traceback.traceback_text:
+        return 'tracebacks-social'
+    elif 'adwords' in traceback.traceback_text:
+        return 'tracebacks-adwords'
+    else:
+        return 'tracebacks'
