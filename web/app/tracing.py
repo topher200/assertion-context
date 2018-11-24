1e73a2b557cb3970d79a23a697ebf044c60b4faa
Topher Brown <topher200@gmail.com>
2018-11-24 06:28:13 +0000

    use env vars to find jaeger agent service

--- web/app/tracing.py
+++ web/app/tracing.py
@@ -1,6 +1,10 @@
 from opentracing_instrumentation.client_hooks import install_all_patches
 import jaeger_client
 
+from . import config_util
+
+JAEGER_REPORTING_HOST = config_util.get('JAEGER_REPORTING_HOST')
+
 
 def initialize_tracer():
     install_all_patches()
@@ -9,6 +13,9 @@ def initialize_tracer():
         config = {
             'sampler': {'type': 'const', 'param': 1},
             'logging': False,
+            'local_agent': {
+                'reporting_host': JAEGER_REPORTING_HOST,
+            }
         },
         service_name='tracebacks'
     )
