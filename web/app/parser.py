be6c6cd0c394bf37da8149a226bd7c0d4e2b4fc4
Topher Brown <t.brown@wordstream.com>
2019-03-10 15:07:41 -0400

    Revert "ignore spammy Assertion"
    This reverts commit f50d9ff21362da865a98bf1bdf2f4055db0e09fa.

--- web/app/parser.py
+++ web/app/parser.py
@@ -16,7 +16,7 @@ from .traceback import Traceback
 
 ERROR_REGEX = re.compile('(?:AssertionError|KeyError|NotImplementedError|ValueError|AttributeError|LockFailed)(?:$|:)')
 ASSERTION_ERROR_REGEX_NEGATIVE = re.compile(
-    '''can only join a child process|DEBUG|can only test a child process|vendor_textad_criterion_id'''
+    '''can only join a child process|DEBUG|can only test a child process'''
 )
 KEY_ERROR_REGEX_NEGATIVE = re.compile(
     '''threading.pyc|args:\[|Process worker'''
