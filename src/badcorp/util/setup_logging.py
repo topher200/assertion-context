dae91a39ef1ba0a0775af6bcf66dcd9188541bc7
Topher Brown <t.brown@wordstream.com>
2019-04-21 00:53:44 -0400

    adapt to a log formatter like $company's

--- src/badcorp/util/setup_logging.py
+++ src/badcorp/util/setup_logging.py
@@ -3,19 +3,22 @@ import logging
 logger = logging.getLogger()
 
 
+__LINE_FORMATTER = logging.Formatter(
+    '%(asctime)s.%(msecs)03d %(process)d/%(threadName)-40s: %(levelname)-8s %(name)s: %(message)s',
+    '%d/%b/%Y:%H:%M:%S')
+
+
 def setup_logging(log_file):
     logger.setLevel(logging.DEBUG)
-    format = '%(asctime)s BadCorp: %(message)s'
-    formatter = logging.Formatter(format, datefmt='%b %d %H:%M:%S')
 
     # add log handler to sys.stderr
     stream_handler = logging.StreamHandler()
-    stream_handler.setFormatter(formatter)
+    stream_handler.setFormatter(__LINE_FORMATTER)
     logger.addHandler(stream_handler)
 
     # output logs to file
     file_handler = logging.FileHandler(log_file)
-    file_handler.setFormatter(formatter)
+    file_handler.setFormatter(__LINE_FORMATTER)
     logger.addHandler(file_handler)
 
     logger.debug('logger configured')
