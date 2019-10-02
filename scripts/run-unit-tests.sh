269038a40af22cff6d1e7219464f105580976dd7
Topher Brown <t.brown@wordstream.com>
2019-10-02 18:52:44 -0400

    run unit tests with env vars

---
+++ scripts/run-unit-tests.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# import environment variables for server.py
+set -a
+source .env
+set +a
+
+# import environment variables pointing to Badcorp services
+set -a
+source .badcorp.env
+set +a
+
+# run the tests
+nosetests --py3where web --quiet
