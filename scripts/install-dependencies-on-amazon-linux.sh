84c516adb3795409f1c10f80b5b865719acc68d0
Topher Brown <t.brown@wordstream.com>
2019-10-18 18:03:07 -0400

    add script for setting up AMI instance

---
+++ scripts/install-dependencies-on-amazon-linux.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# installs the dependencies necessary to run docker-compose from a fresh Amazon
+# Linux box. use this to create a fresh AMI
+
+# install docker (https://docs.aws.amazon.com/AmazonECS/latest/developerguide/docker-basics.html#install_docker)
+sudo amazon-linux-extras install -y docker
+sudo service docker start
+sudo usermod -a -G docker ec2-user
+
+# install docker-compose (https://docs.docker.com/compose/install/)
+sudo curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+# install dynaconf, for configuration handling
+sudo yum install -y python3
+sudo pip3 install dynaconf
