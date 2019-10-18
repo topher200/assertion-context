da96ad268a996c60415bc3e200bd9dedab14aef4
Topher Brown <t.brown@wordstream.com>
2019-10-18 18:45:09 -0400

    auto-start docker on EC2 image restart

--- scripts/install-dependencies-on-amazon-linux.sh
+++ scripts/install-dependencies-on-amazon-linux.sh
@@ -7,6 +7,7 @@
 sudo amazon-linux-extras install -y docker
 sudo service docker start
 sudo usermod -a -G docker ec2-user
+sudo systemctl enable docker
 
 # install docker-compose (https://docs.docker.com/compose/install/)
 sudo curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
