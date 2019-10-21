a94355aa804cf94b37d09150e65330b8b58c0722
Topher Brown <t.brown@wordstream.com>
2019-10-21 14:08:17 -0400

    add papertrail logging install to AMI

--- scripts/install-dependencies-on-amazon-linux.sh
+++ scripts/install-dependencies-on-amazon-linux.sh
@@ -13,6 +13,12 @@ sudo systemctl enable docker
 sudo curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
+# install papertrail logging
+aws s3 cp s3://tracebacks-configuration/papertrail-logging-destination ./papertrail-logging-destination
+docker run --restart=always -d \
+       -v=/var/run/docker.sock:/var/run/docker.sock gliderlabs/logspout  \
+       syslog+tls://$(cat ./papertrail-logging-destination)
+
 # install dynaconf, for configuration handling
 sudo yum install -y python3
 sudo pip3 install dynaconf
